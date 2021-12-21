e62e3f9e277d693ed4140b13d936bcd736038708
Betty Han <xiaolei_han@yahoo.ca>
2021-12-20 19:36:14 -0500

    Print prime numbers 1-100

---
+++ primeNum.py
@@ -0,0 +1,16 @@
+def isPrime(n):
+    if n <= 1:
+        return False
+    for x in range(2, n):
+        if n % x == 0:
+            return False
+    else:
+        return True
+
+
+for number in range(1, 101):
+    if isPrime(number):
+        print(number)
+
+
+
