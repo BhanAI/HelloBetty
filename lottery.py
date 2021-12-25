a400065843780c8a92b51621f693dbedf44da854
Betty Han <xiaolei_han@yahoo.ca>
2021-12-25 14:46:13 -0500

    Add lottery numbers

---
+++ lottery.py
@@ -0,0 +1,22 @@
+import random
+
+class LotteryGenerator:
+
+    numbers = []
+
+    def gerRandomNumber(self):
+        return random.randint(1, 49)
+
+    def getFinalNumbers(self):
+        i = 0
+        while i < 7:
+            y = self.gerRandomNumber()
+            if y not in self.numbers:
+                self.numbers.append(y)
+                i +=1
+        return self.numbers
+
+
+lotteryGenerator = LotteryGenerator()
+n = lotteryGenerator.getFinalNumbers()
+print(n)
